38adf2170a5ab15a71ffe3b91b1260c512b14924
ridwanyah23 <ridwangames24@gmail.com>
2024-06-16 06:53:05 +0100

    the signup form

---
+++ secure-exchange/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import SignUpForm from "./ui/signup";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-blue-100">
+      {/* <SignUpFrm/> */}
+    </main>
+  );
+}
