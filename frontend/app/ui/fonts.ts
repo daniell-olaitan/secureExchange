dbcf4db7b8dda2b436695b0fd966690d7c5cbc88
ridwanyah23 <ridwangames24@gmail.com>
2024-06-19 22:43:37 +0100

    n

--- frontend/app/ui/fonts.ts
+++
@@ -1,6 +0,0 @@
-import { Inter, Lusitana , Roboto} from "next/font/google";
-
-
-export const inter = Inter({subsets:['latin']})
-export const roboto = Roboto({subsets:['latin'], weight: '700'})
-export const lusitana = Lusitana({subsets:['latin'], weight:'700'})
--- secure-exchange/app/ui/dashboard/navigation.tsx
+++ secure-exchange/app/ui/dashboard/navigation.tsx
@@ -15,8 +15,8 @@ export default function HomeNav(){
         <div className='flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2'>
         <NavLinks/>
         <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block "></div>
-          <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-red-700
-           md:flex-none md:justify-start md:p-2 md:px-3  bg-blue-700">
+          <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-blue-500 p-3 text-sm font-medium hover:bg-red-700
+           md:flex-none md:justify-start md:p-2 md:px-3  md:bg-blue-700">
             <PowerIcon className="w-6" />
             <div className="hidden md:block">Sign Out</div>
           </button>
